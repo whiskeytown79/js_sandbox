4f09b39478ebb4bd118b4f9d596de692ec5b86ab
Jake Cohen <cohen.jacob@gmail.com>
2021-02-25 11:15:14 -0800

    Add more trapping-rain-water test cases

--- src/trapping-rain-water.test.js
+++ src/trapping-rain-water.test.js
@@ -7,3 +7,23 @@ test('example 1 from leetcode', () => {
 test('example 2 from leetcode', () => {
     expect(trappingRainWater([4,2,0,3,2,5])).toBe(9);
 });
+
+test('bathtub curve heights', () => {
+    expect(trappingRainWater([4, 1, 1, 1, 1, 4])).toBe(12);
+});
+
+test('no valleys', () => {
+    expect(trappingRainWater([1,2,3,4,3,2,1])).toBe(0);
+});
+
+test('two equal valleys', () => {
+    expect(trappingRainWater([1, 3, 5, 3, 2, 5, 2, 3, 5, 3, 1])).toBe(10);
+});
+
+test('ascending valleys', () => {
+    expect(trappingRainWater([1,2,3,2,3,4,3,4,5,4,5,6,5,4,3,2,1])).toBe(3);
+});
+
+test('descending valleys', () => {
+    expect(trappingRainWater([1,2,3,4,5,6,5,4,5,4,3,4,3,2,3,2,1])).toBe(3);
+});
