ead2d8295dc4df4e77728a11c2a6ab0d1fb228bd
Jake Cohen <cohen.jacob@gmail.com>
2021-02-28 17:13:19 -0800

    Switch approach, don't merge the arrays, just find what would be the midpoint if they were merged. There is still a log(m+n) solution, but I dont have it yet

--- src/median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ src/median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -3,30 +3,54 @@
  * @param {number[]} nums2
  * @return {number}
  */
+/*
+Idea: to speed this up, don't zip the array, but go through the loop as if I'm zipping the array
+up to the midpoint or pair of midpoints (known in advance by the summed length of the arrays).
+I either then get the one value I want, or a pair of values to average.
+ */
 function findMedianSortedArrays(nums1, nums2) {
-    let merged = [];
     let i1 = 0;
     let i2 = 0;
+    let totalLength = nums1.length + nums2.length;
+    let target1 = Math.floor((totalLength - 1) / 2);
+    let target2 = Math.floor(totalLength / 2);
+    let val1 = 0;
+    let val2 = 0;
+    let pos = 0;
     while (i1 < nums1.length || i2 < nums2.length) {
         if (i1 < nums1.length) {
             if (i2 < nums2.length && nums2[i2] < nums1[i1]) {
-                merged.push(nums2[i2]);
+                if (pos == target1) {
+                    val1 = nums2[i2];
+                }
+                if (pos == target2) {
+                    val2 = nums2[i2];
+                }
                 i2 += 1;
             } else {
-                merged.push(nums1[i1]);
+                if (pos == target1) {
+                    val1 = nums1[i1];
+                }
+                if (pos == target2) {
+                    val2 = nums1[i1];
+                }
                 i1 += 1;
             }
         } else if (i2 < nums2.length) {
-            merged.push(nums2[i2]);
+            if (pos == target1) {
+                val1 = nums2[i2];
+            }
+            if (pos == target2) {
+                val2 = nums2[i2];
+            }
             i2 += 1;
         }
+        pos = pos + 1;
+        if (pos > target2) {
+            break;
+        }
     }
-    if (merged.length % 2) {
-        return merged[Math.floor(merged.length / 2)];
-    }
-    let left = merged[Math.floor(merged.length / 2) - 1];
-    let right = merged[Math.floor(merged.length / 2)];
-    return (left + right) / 2;
+    return (val1 + val2) / 2;
 }
 
 module.exports = findMedianSortedArrays;
