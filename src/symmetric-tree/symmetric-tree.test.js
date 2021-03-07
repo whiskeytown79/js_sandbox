75d6b47ed97d4162c2010e1ed66ea1f1320730b6
Jake Cohen <cohen.jacob@gmail.com>
2021-03-07 02:25:37 -0800

    Update some test cases with new examples from leetcode

--- src/symmetric-tree/symmetric-tree.test.js
+++ src/symmetric-tree/symmetric-tree.test.js
@@ -10,3 +10,18 @@ test('leetcode example 2', () => {
     let tree = makeTree([1,2,2,null,3,null,3]);
     expect(isSymmetric(tree)).toBe(false);
 });
+
+test('leetcode test case 1', () => {
+    let tree = makeTree([1, 2, 2, null, 3, 3]);
+    expect(isSymmetric(tree)).toBe(true);
+});
+
+test('leetcode test case 2', () => {
+    let tree = makeTree([2,3,3,4,5,5,4,null,null,8,9,9,8]);
+    expect(isSymmetric(tree)).toBe(true);
+});
+
+test('leetcode test case 3', () => {
+    let tree = makeTree([1]);
+    expect(isSymmetric(tree)).toBe(true);
+});
