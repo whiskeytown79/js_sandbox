b5f4fa1f21b8908a15a32e533bb19e7e731b07ad
Jake Cohen <cohen.jacob@gmail.com>
2021-03-11 00:04:02 -0800

    Much faster by adding a couple optimizations: skip processing when my current num is >0, and skip processing duplicate numbers

--- src/3sum/3sum.js
+++ src/3sum/3sum.js
@@ -8,6 +8,8 @@ function threeSum(nums) {
     let seen = {};
 
     for (let i=0; i<nums.length-2; i++) {
+        if (nums[i] > 0) break;
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
         let j = i + 1;
         let k = nums.length - 1;
         while (j < k) {
