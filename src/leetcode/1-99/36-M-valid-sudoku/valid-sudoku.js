a0e3a0af33d0e21196cb2e76fc0fdadfe41e6955
Jake Cohen <cohen.jacob@gmail.com>
2021-03-23 01:03:25 -0700

    Switch to single pass with hashes for sudoku verify

--- src/leetcode/1-99/36-M-valid-sudoku/valid-sudoku.js
+++ src/leetcode/1-99/36-M-valid-sudoku/valid-sudoku.js
@@ -1,45 +1,28 @@
-function checkNums(row) {
-    let hash = {};
-    for (let col=0; col<row.length; col++) {
-        if (row[col] === '.') continue;
-        let num = parseInt(row[col]);
-        if (num < 1 || num > 9) return false;
-        if (hash[num]) return false;
-        hash[num] = true;
-    }
-    return true;
-}
-
-
 /**
  * @param {string[][]} board
  * @return {boolean}
  */
 function isValidSudoku(board) {
-    // Check rows
-    for (let row=0; row<board.length; row++) {
-        if (!checkNums(board[row])) {
-            return false;
-        }
+    let rowHash = {};
+    let colHash = {};
+    let gridHash = {};
+    for (let i=0; i<9; i++) {
+        rowHash[i] = {};
+        colHash[i] = {};
+        gridHash[i] = {};
     }
 
-    // Check cols
-    for (let col=0; col<board[0].length; col++) {
-        if (!checkNums(board.map(x => x[col]))) {
-            return false;
-        }
-    }
-
-    // Check 3x3 subgrids
-    for (let row=0; row<board.length; row += 3) {
-        for (let col=0; col<board[0].length; col += 3) {
-            if (!checkNums([
-                board[row][col], board[row][col+1], board[row][col+2],
-                board[row+1][col], board[row+1][col+1], board[row+1][col+2],
-                board[row+2][col], board[row+2][col+1], board[row+2][col+2]
-            ])) {
-                return false;
-            }
+    for (let i=0; i<9; i++) {
+        for (let j=0; j<9; j++) {
+            let n = board[i][j];
+            if (n === '.') continue;
+            if (rowHash[i][n]) return false;
+            rowHash[i][n] = true;
+            if (colHash[j][n]) return false;
+            colHash[j][n] = true;
+            let gridIdx = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+            if (gridHash[gridIdx][n]) return false;
+            gridHash[gridIdx][n] = true;
         }
     }
 
