c9e9bc5380a1ec2939773a531acf54f19e422628
Jake Cohen <cohen.jacob@gmail.com>
2021-03-30 23:49:16 -0700

    Add comment on basic-calculator

--- src/leetcode/100-999/221-H-basic-calculator/basic-calculator.js
+++ src/leetcode/100-999/221-H-basic-calculator/basic-calculator.js
@@ -1,8 +1,21 @@
 /**
+ * Calculate a simple arithmetic expression, where + and - are the supported operations,
+ * as well as unary - for negative numbers. Parentheses are supported. The input is expected
+ * to be a valid expression (leetcode guarantees this).
+ *
+ * This approach uses the fact that + and - have the same precedence and are associative. This
+ * allows evaluating numbers and operations by flipping a "sign" bit every time we see a
+ * - character, and then always performing addition (modified by the sign bit) when we see a number.
+ *
+ * Parentheses are handled by using a stack to push a new working total and sign bit, then popping
+ * them off and adding that running total to the previous stack frame.
+ * 
  * @param {string} s
  * @return {number}
  */
 function calculate(s) {
+    // Break input string into tokens, discarding whitespace, and converting
+    // runs of digits into a single token.
     let tokens = s.match(/\(|\)|\+|-|[0-9]+/g);
 
     let stack = [{total: 0, neg: false}];
